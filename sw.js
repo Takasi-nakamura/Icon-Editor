889581411870db9a5e2feaf91271f94028e511f3
Takasi-nakamura <haruto.yomogida@gmail.com>
2026-09-05 15:04:11 +0900

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='icon-editor-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
+self.addEventListener('activate',event=>{event.waitUntil(self.clients.claim())});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)))});
